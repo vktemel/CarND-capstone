b9858e718bb6b5ec170f54631014ab5749b7ea6f
vktemel <vktemel@umich.edu>
2021-03-14 10:28:19 -0700

    improved the braking logic and used more vehicle parameters

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -9,14 +9,21 @@ import rospy
 
 
 class Controller(object):
-    def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
+    def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle, decel_limit, wheel_radius, vehicle_mass):
         self.throttle = 0
 
-        self.pid_speed = PID(0.2, 0, 0.1)
+        self.pid_speed = PID(0.2, 0.001, 0.1)
 
         self.yawCtrl = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
 
         self.timestamp = rospy.get_time()
+
+        self.decel_limit = decel_limit
+        self.wheel_radius = wheel_radius
+        self.vehicle_mass = vehicle_mass
+        
+    def reset(self):
+        self.pid_speed.reset()
 
     def control(self, target_linear_vel, target_angular_vel, current_linear_vel, current_angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
@@ -33,9 +40,18 @@ class Controller(object):
             throttle = 0
 
         steer = self.yawCtrl.get_steering(target_linear_vel.x, target_angular_vel.z, current_linear_vel.x)
+        
+        # rospy.logwarn("target vel x: %s", target_linear_vel.x)
 
-        if((current_linear_vel.x < 0.1) & (target_linear_vel < 0.1)):
+        vel_delta = current_linear_vel.x - target_linear_vel.x
+
+        if((current_linear_vel.x < 0.01) & (target_linear_vel.x == 0.0)):
+            throttle = 0
             brake = 700
+        elif(vel_delta > 0.1):
+            throttle = 0
+            decel = max(vel_delta, self.decel_limit)
+            brake = abs(decel)*self.vehicle_mass*self.wheel_radius
         else:
             brake = 0.0
 
